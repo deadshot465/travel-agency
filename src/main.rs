11cf0e7939d0ea6fd88fc727b1f8e61f71cd8965
Chehui Chou <deadshot465@gmail.com>
2025-06-07 04:38:26 +0900

    Validate signature for Discord interactions.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use axum::{Router, routing::post};
+use axum::{Router, middleware::from_fn, routing::post};
 use tracing::Level;
 
 use crate::{
-    controller::discord::interaction::handle_interaction, shared::structs::config::CONFIGURATION,
+    controller::discord::interaction::handle_interaction,
+    shared::middleware::discord_validation::validate_interaction,
 };
 
 mod controller;
@@ -10,7 +11,7 @@ mod shared;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let log_level = match CONFIGURATION.log_level.as_str() {
+    let log_level = match std::env::var("LOG_LEVEL")?.as_str() {
         "TRACE" => Level::TRACE,
         "INFO" => Level::INFO,
         "WARN" => Level::WARN,
@@ -30,9 +31,15 @@ async fn main() -> anyhow::Result<()> {
         );
     }
 
-    let app = Router::new().route("/api/discord/interaction", post(handle_interaction));
+    let app = Router::new()
+        .route("/api/discord/interaction", post(handle_interaction))
+        .layer(from_fn(validate_interaction));
 
-    let listener = tokio::net::TcpListener::bind(&CONFIGURATION.server_bind_point).await?;
+    let server_bind_point = std::env::var("SERVER_BIND_POINT")?;
+    let port = std::env::var("PORT")?;
+    let server_bind_point = format!("{}:{}", server_bind_point, port);
+
+    let listener = tokio::net::TcpListener::bind(&server_bind_point).await?;
     axum::serve(listener, app).await?;
 
     Ok(())
---
+++ src/shared/middleware/discord_validation.rs
@@ -0,0 +1,104 @@
+use axum::{
+    body::{Body, Bytes},
+    http::HeaderMap,
+    middleware::Next,
+    response::{IntoResponse, Response},
+};
+use http_body_util::BodyExt;
+
+const SIGNATURE_HEADER: &str = "X-Signature-Ed25519";
+const TIMESTAMP_HEADER: &str = "X-Signature-Timestamp";
+
+pub async fn validate_interaction(
+    headers: HeaderMap,
+    request: axum::extract::Request,
+    next: Next,
+) -> Response {
+    let signature = headers
+        .get(SIGNATURE_HEADER)
+        .cloned()
+        .and_then(|v| v.to_str().map(ToString::to_string).ok())
+        .unwrap_or_default();
+
+    tracing::debug!("Signature: {}", &signature);
+
+    let timestamp = headers
+        .get(TIMESTAMP_HEADER)
+        .cloned()
+        .and_then(|v| v.to_str().map(ToString::to_string).ok())
+        .unwrap_or_default();
+
+    tracing::debug!("Timestamp: {}", &timestamp);
+
+    match buffer_request_body(request, signature, timestamp).await {
+        Ok(request) => next.run(request).await,
+        Err(e) => e,
+    }
+}
+
+async fn buffer_request_body(
+    request: axum::extract::Request,
+    signature: String,
+    timestamp: String,
+) -> Result<axum::extract::Request, Response> {
+    let (parts, body) = request.into_parts();
+
+    let bytes = body
+        .collect()
+        .await
+        .map_err(|e| {
+            let error_msg = format!("Internal server error when collecting body bytes: {}", e);
+            tracing::error!("{}", &error_msg);
+            (axum::http::StatusCode::INTERNAL_SERVER_ERROR, error_msg).into_response()
+        })?
+        .to_bytes();
+
+    match validate(bytes, signature, timestamp) {
+        Ok(bytes) => Ok(axum::extract::Request::from_parts(parts, Body::from(bytes))),
+        Err(e) => Err(e),
+    }
+}
+
+fn validate(bytes: Bytes, signature: String, timestamp: String) -> Result<Bytes, Response> {
+    let public_key =
+        std::env::var("APPLICATION_PUBLIC_KEY").expect("Failed to get application public key.");
+
+    tracing::debug!("Public key: {}", &public_key);
+
+    let body = bytes.to_vec();
+
+    match String::from_utf8(body) {
+        Ok(s) => {
+            tracing::debug!("String body: {}", &s);
+            let message = format!("{}{}", timestamp, s);
+
+            let signature_bytes =
+                hex::decode(&signature).expect("Failed to decode public key from hex value.");
+            let public_key_bytes =
+                hex::decode(&public_key).expect("Failed to decode public key from hex value.");
+
+            let result =
+                nacl::sign::verify(&signature_bytes, message.as_bytes(), &public_key_bytes);
+
+            match result {
+                Ok(res) => {
+                    if res {
+                        Ok(bytes)
+                    } else {
+                        Err(axum::http::StatusCode::UNAUTHORIZED.into_response())
+                    }
+                }
+                Err(e) => {
+                    let error_msg = format!("Failed to verify: {:?}", e);
+                    tracing::error!("{}", &error_msg);
+                    Err((axum::http::StatusCode::INTERNAL_SERVER_ERROR, error_msg).into_response())
+                }
+            }
+        }
+        Err(e) => {
+            let error_msg = format!("Failed to build string from UTF-8 encoded body: {}", e);
+            tracing::error!("{}", &error_msg);
+            Err((axum::http::StatusCode::INTERNAL_SERVER_ERROR, error_msg).into_response())
+        }
+    }
+}
